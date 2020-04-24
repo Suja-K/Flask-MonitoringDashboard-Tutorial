e8c0e5f3685a263095e2d18dbd33793b9917a8f3
Johan Settlin <settlin@kth.se>
2020-04-24 11:30:51 +0200

    Add config.cfg file

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 import flask_monitoringdashboard as dashboard
 
 app = Flask(__name__)
+dashboard.config.init_from(file='config.cfg')
+# Make sure to call config.init_from() before bind()
 dashboard.bind(app)
 
 @app.route('/')
